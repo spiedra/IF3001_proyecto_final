251f0978b61d7fc8217a25eed422fe1592ccfeed
spiedra <jcspiedra@gmail.com>
2021-06-07 15:43:44 -0600

    added conexionSqlServer clas

---
+++ IF3001_proyecto_final/IF3001_proyecto_final/Data/ConexionSqlServerData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace IF3001_proyecto_final.Data
+{
+    class ConexionSqlServerData
+    {
+        private SqlCommand sqlCommand;
+        private SqlConnection sqlConnection;
+        private SqlDataReader sqlDataReader;
+
+        public ConexionSqlServerData()
+        {
+            this.ConnectToDatabase();
+        }
+
+        public object ConnectToDatabase()
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(GetConnectionString());
+                return sqlConnection;
+            }
+            catch (SqlException sqlException)
+            {
+                return sqlException.Number;
+            }
+        }
+
+        static private string GetConnectionString()
+        {
+            return "Data Source=163.178.107.10; database=IF4101_B97452_LAB1; User Id=laboratorios; Password=KmZpo.2796";
+        }
+    }
+}
