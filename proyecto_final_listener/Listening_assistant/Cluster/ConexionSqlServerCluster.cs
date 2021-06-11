ac895a895a22462649b13e13f2505141ef6a2ea3
Randall <randms07@hotmail.com>
2021-06-11 16:41:38 -0600

    agregando app de listener

---
+++ proyecto_final_listener/Listening_assistant/Cluster/ConexionSqlServerCluster.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace IF3001_proyecto_final.Cluster
+{
+    class ConexionSqlServerCluster
+    {
+        public ConexionSqlServerCluster()
+        {
+
+        }
+
+        public object ConnectToDatabase()
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(GetConnectionString());
+                sqlConnection.Open();
+                sqlConnection.Close();
+                return sqlConnection;                
+            }
+            catch (SqlException sqlException)
+            {
+                return null;
+            }
+        }
+
+        static private string GetConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "35.193.110.243";
+            builder.UserID = "sqlserver";
+            builder.Password = "piedra";
+            builder.InitialCatalog = "testfgfgf";
+            return builder.ConnectionString;
+        }
+    }
+}
